bf5b226a59a74e189947cbc2d0ec0c57bc1da069
AlexadreNoguez <alexandrenoguez_rs@yahoo.com.br>
2024-01-12 22:26:40 -0300

    feat: Created user DTOs

---
+++ src/main/java/com/sistemapacto/server/dto/user/UserCreateDTO.java
@@ -0,0 +1,23 @@
+package com.sistemapacto.server.dto.user;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import javax.validation.constraints.NotBlank;
+
+public class UserCreateDTO {
+    @Schema(description = "Login do usuário", example = "Alexandre.Noguez")
+    @NotBlank(message = "Campo obrigatório, verifique e tente novamente.")
+    private String userLogin;
+
+    @Schema(description = "Senha do usuário", example = "12365185436")
+    @NotBlank(message = "Campo obrigatório, verifique e tente novamente.")
+    private String userPassword;
+
+    @Schema(description = "Email do usuário", example = "alexandrenoguez@email.com")
+    @NotBlank(message = "Campo obrigatório, verifique e tente novamente.")
+    private String userEmail;
+
+    @Schema(description = "Cargo do usuário", example = "Recrutador ou Candidato")
+    @NotBlank(message = "Campo obrigatório, verifique e tente novamente.")
+    private String userRole;
+}
